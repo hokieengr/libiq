e6296659ce810cfdf0683229a65a1a467b76d3c5
hokieengr <email.customs+git@gmail.com>
2018-11-28 02:25:17 -0500

    More roughing out of structure.

--- include/libiq.h
+++ include/libiq.h
@@ -1,90 +1,57 @@
 #ifndef __LIBIQ_H__
 #define __LIBIQ_H__
 
-/* Flags */
-#define LIBIQ_RDONLY 0 // fix actual values
-#define LIBIQ_RW 0 // fix actual values
-#define LIBIQ_WRITE 0 // fix actual values
-#define LIBIQ_APPEND 0 // fix actual values
 
-/* FILEIQ Type */
-
-/* maybe predefined structures to help */
-typedef struct {
-    uint8_t i;
-    uint8_t q;
-} iq_samp_u8_t;
-
-typedef struct {
-    float i;
-    float q;
-} iq_samp_f32_t;
-
-
-
-
-/* Maybe user stuff */
 #define   SAMPLE_DTYPE_UINT8   0x01
 #define   SAMPLE_DTYPE_F32     0x02
 
-/* not user stuff */
-#define   BLOCK_SIZE_BYTES     1048576 /* number of bytes to compress at a time */
 
+/* move this to .c file if possible */
 typedef struct {
-    FILE *fp;
-    uint32_t frequency;
-    uint32_t samp_rate;
-    int samp_type;
+    FILE       *fp;
+    uint32_t    tuner_freq;
+    uint32_t    samp_rate;
+    int         samp_type;
     /* compression buffers */
 } FILEIQ;
 
 
+/*
+ * Routine to open file for writing/reading
+ */
+FILEIQ *
+libiq_open(const char *filename, const char *mode, uint32_t tuner_freq, 
+           uint32_t samp_rate, int samp_type);
 
-/* Also need option for compression */
-
-/* Open file (including specifying frequency and other things */
-FILEIQ *libiq_open(const char *filename, uint16_t flags);
-/* OR */
-
-
-FILEIQ *libiq_open(const char *filename, const char *mode); // this doesn't handle types
-// flags handle multiple types and actiosn
-
-FILEIQ *fin;
-
+/*
+ * Some useage examples for future reference
+ *
 fout = libiq_open("outputfile.iq", "w", 857000000, 2400000, SAMPLE_DTYPE_UINT8)
 fout = libiq_open("outputfile.iq", "rw", 857000000, 2400000, SAMPLE_DTYPE_F32)
 fin = libiq_open("outputfile.iq", "r", NULL, NULL);
+*/
 
 
-/* An fwrite() type write function */
-size_t libiq_write(const void *buf, size_t nsamp, FILEIQ* fout);
-
-multipe writes for different types? or different opens?
-
-/* An fread type read function */
-size_t libiq_read(void *ptr, size_t nsamp, FILEIQ* fin);
-
-/* Close file */
-int libiq_close(FILEIQ *f);
+/* 
+ * Write to file
+ */
+size_t
+libiq_write(const void *buf, size_t nsamp, FILEIQ* fout);
 
 
 
-/* Also need option for compression */
-
-/* Open file (including specifying frequency and other things */
-FILEIQ *libiq_open(const char *filename, uint16_t flags);
-/* OR */
-FILEIQ *libiq_open(const char *filename, const char *mode, uint8_t  flags);
-
-/* An fwrite() type write function */
-size_t libiq_write(const void *buf, size_t nsamp, FILEIQ* fout);
-
-/* An fread type read function */
-size_t libiq_read(void *ptr, size_t nsamp, FILEIQ* fin);
+/*
+ * Read from file
+ */
+size_t
+libiq_read(void *ptr, size_t nsamp, FILEIQ* fin);
 
 /* Close file */
-int libiq_close(FILEIQ *f);
+int
+libiq_close(FILEIQ *f);
+
+
+
 
 #endif /* __LIBIQ_H__ */
 
--- src/libiq.c
+++ src/libiq.c
@@ -1,3 +1,40 @@
-
 #include "libiq.h"
+
+/* number of bytes to compress at a time */
+#define   BLOCK_SIZE_BYTES     1048576
+
+#define   COMP_NONE   0x00
+#define   COMP_ZLIB   0x01
+
+
+/* 
+ * File header. Try to keep in mind the common problem of losing power, filling
+ * up a tiny memory card, etc when designing this.
+ */
+typedef struct
+{
+    uint32_t id;
+    uint32_t version;
+    uint32_t tuner_freq;
+    uint32_t samp_rate;
+    uint32_t samp_type;
+} filehdr_t;
+    
+
+
+/*
+ * Data block header
+ */
+typedef struct
+{
+    uint32_t id;
+    uint32_t flags; /* use to specify which compression algorithm */
+    uint32_t crc;
+    uint32_t nbytes_compressed;
+    uint32_t nbytes_raw;
+} blockhdr_t;
+
+
+
+
 
